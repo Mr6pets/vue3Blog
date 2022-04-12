5682a45733e63b87b6fabab68513cb1ffec6fa54
Mr6pets <alvispets@gmail.com>
2022-04-12 11:46:54 +0800

    ..

---
+++ blog/src/views/home.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import {ref} from 'vue';
+
+  let name="liu";
+  let age="28";
+
+  let handleClick=()=>{
+    name="oo"
+  }
+</script>
+
+<template>
+  <div>
+    <p>{{name}}-{{age}}</p>
+    <button @click="handleClick"></button>
+  </div>
+</template>
+
+<style>
+
+</style>
