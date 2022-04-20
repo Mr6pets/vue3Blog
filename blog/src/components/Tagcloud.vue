bdfe3da655063825dc075fdb3c8956d1ef3220e7
Mr6pets <alvispets@gmail.com>
2022-04-20 16:41:45 +0800

    ..

--- blog/src/components/Tagcloud.vue
+++ blog/src/components/Tagcloud.vue
@@ -1,17 +1,49 @@
 <script setup>
 import {ref} from 'vue';
+const tags=ref([]);
+const tagSet=new Set();
 
-  defineProps({
-    posts:Array
+const props= defineProps({
+  posts:Array
+})
+props.posts.forEach(item=>{
+  item.tags.forEach(tag=>{
+    tagSet.add(tag);
   })
+})
+tags.value=[...tagSet];
 </script>
 
 <template>
-  <div>
-    {{post}}
+  <div class="tag-cloud">
+    <h3>标签</h3>
+    <div v-for="(item,index) in tags" :key="index">
+      <router-link :to="{name:'TagCloud', params:{item}}"> #{{item}}</router-link>
+      </div>
+    
   </div>
 </template>
 
 <style scoped>
 
+.tag-cloud{
+  padding: 10px;
+}
+.tag-cloud h3{
+  border-bottom: 1px solid #eee;
+  padding: 16px 8px;
+  color: #444;
+}
+.tag-cloud div{
+  display: inline-block;
+  padding: 10px;
+}
+.tag-cloud a{
+  color: #ccc;
+  text-decoration: none;
+}
+.tag-cloud a.router-link-active{
+  color: #ff8800;
+  font-weight: bold;
+}
 </style>
---
+++ blog/src/views/tag.vue
@@ -0,0 +1,25 @@
+<script setup>
+  import getposts from "../composibles/getpost"
+  import {useRoute} from 'vue-router';
+  import {computed} from "vue";
+  import PostList from '../components/postList.vue'
+  const route=useRoute();
+
+  const {posts,load} =getposts();
+  load();
+  
+  const withTag=computed(()=>{
+    return posts.value.filter((p)=> p.tags.includes(route.params.item))
+  })
+</script>
+
+<template>
+  <div class="tag">
+    <div v-if="posts.length">
+      <PostList :posts="withTag" />
+    </div>
+  </div>
+</template>
+<style scoped>
+
+</style>
